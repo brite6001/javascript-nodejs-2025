6b0563f62e150f027ce0a561d92acdaa647bdeca
mana <manashe.tsx@gmail.com>
2025-03-25 19:42:50 -0300

    literal objects

--- 01-fundamentos/12-literal-objects/main.js
+++ 01-fundamentos/12-literal-objects/main.js
@@ -33,3 +33,19 @@ console.log(personaje);
 // Object.entries(personaje) convierte el objeto personaje en un array de pares clave-valor
 const entriesPares = Object.entries(personaje);
 console.log("entriesPares:", entriesPares);
+
+// Object.freeze(personaje) hace que el objeto persona sea inmutable, lo que significa que:
+// No se pueden agregar nuevas propiedades al objeto.
+// No se pueden eliminar propiedades del objeto.
+// No se pueden modificar los valores de las propiedades existentes.
+// No se puede cambiar el prototipo del objeto.
+Object.freeze(personaje);
+personaje.dinero = 1000000000;
+console.log(personaje);
+
+// Listado de todas las propiedades del objeto
+const propiedades = Object.getOwnPropertyNames(personaje);
+console.log("Propiedades:", propiedades);
+
+const valores = Object.values(personaje);
+console.log("valores:", valores);
