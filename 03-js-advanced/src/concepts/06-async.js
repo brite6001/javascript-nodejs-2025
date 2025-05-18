c8dc545f970a5994a5c99117d0ac803e86bb3887
manatsx <manashe.tsx@gmail.com>
2025-05-18 20:14:43 +0000

    async

--- 03-js-advanced/src/concepts/06-async.js
+++ 03-js-advanced/src/concepts/06-async.js
@@ -1,5 +1,29 @@
+import { heroes } from "../api/heroes.js";
+
+/**
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const asyncComponent = (element) => {
+  const id1 = "5d86371fd55e2e2a30fe1ccb22";
+  console.log("Inicio de componente");
+
+  findHero(id1)
+    .then((name) => (element.innerHTML = name))
+    .catch((error) => (element.innerHTML = error));
+
+  console.log("Fin del componente");
+};
+
 /**
  *
- * @param {HTMLDivElement} element
+ * @param {String} id
+ * @returns {Promise<String>}
  */
-export const asyncComponent = (element) => {};
+const findHero = async (id) => {
+  const hero = heroes.find((hero) => hero.id === id);
+  if (!hero) throw `Hero with id ${id} not found`;
+
+  return hero.name;
+};
