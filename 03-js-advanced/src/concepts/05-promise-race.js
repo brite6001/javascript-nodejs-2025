b8b126c7a899c17db14df9b9fde219953ac63e7b
manatsx <manashe.tsx@gmail.com>
2025-05-18 17:24:04 +0000

    promesas

---
+++ 03-js-advanced/src/concepts/05-promise-race.js
@@ -0,0 +1,35 @@
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const promiseRaceComponent = (element) => {
+  const renderValue = (value) => {
+    element.innerHTML = value;
+  };
+
+  Promise.race([slowPromise, mediumPromise, fastPromise])
+    .then((value) => {
+      renderValue(value);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+const slowPromise = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Slow Promise");
+  }, 2000);
+});
+
+const mediumPromise = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Medium Promise");
+  }, 1500);
+});
+
+const fastPromise = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Fast Promise");
+  }, 1000);
+});
--- 03-js-advanced/src/main.js
+++ 03-js-advanced/src/main.js
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import { environmentsComponent } from "./concepts/01-environments";
 import { callbacksComponent } from "./concepts/02-callbacks";
 import { promiseComponent } from "./concepts/03-promesas";
+import { promiseRaceComponent } from "./concepts/05-promise-race";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -25,4 +26,5 @@ const element = document.querySelector(".card");
 
 // environmentsComponent( element );
 // callbacksComponent(element);
-promiseComponent(element);
+// promiseComponent(element);
+promiseRaceComponent(element);
