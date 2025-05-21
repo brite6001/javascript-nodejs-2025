d5959bd97983fed1451f28e2e1102d7cf00145ac
manatsx <manashe.tsx@gmail.com>
2025-05-21 18:42:15 +0000

    fetch http

--- 04-http-app/src/breakingbad/breaking-bad-app.js
+++ 04-http-app/src/breakingbad/breaking-bad-app.js
@@ -8,6 +8,7 @@ const fetchQuote = async () => {
   }
 
   const data = await response.json();
+  console.log(data[0]);
   return data[0];
 };
 
@@ -15,4 +16,28 @@ const fetchQuote = async () => {
  *
  * @param {HTMLDivElement} element
  */
-export const BreakingbadApp = async (element) => {};
+export const BreakingbadApp = async (element) => {
+  document.querySelector("#app-title").innerHTML = "Breakingbad App";
+  element.innerHTML = "Loading...";
+  // await fetchQuote();
+
+  const quoteLabel = document.createElement("blockquote");
+  const authoLabel = document.createElement("h3");
+  const nextQuoteButton = document.createElement("button");
+  nextQuoteButton.innerText = "Next Quote";
+
+  const renderQuote = (data) => {
+    quoteLabel.innerHTML = data.quote;
+    authoLabel.innerHTML = data.author;
+    element.replaceChildren(quoteLabel, authoLabel, nextQuoteButton);
+  };
+
+  // Añadir listener
+  nextQuoteButton.addEventListener("click", async () => {
+    element.innerHTML = "Loading...";
+    const quote = await fetchQuote();
+    renderQuote(quote);
+  });
+
+  fetchQuote().then(renderQuote);
+};
